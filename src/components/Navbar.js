85af7cbb56d05cfe567853b7f717617d554d966a
Yash Velagapudi <yashv6655@gmail.com>
2020-05-31 18:20:59 -0700

    Navbar change

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,36 +1,41 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import logo from "../images/VacationSpots.png";
-import {FaAlignRight} from "react-icons/fa";
-import {Link} from "react-router-dom";
-
+import { FaAlignRight } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 export default class Navbar extends Component {
-    state = {
-        isOpen:false
-    }
+  state = {
+    isOpen: false,
+  };
 
-    handleToggle = () => {
-        this.setState({isOpen: !this.state.isOpen});
-    }
+  handleToggle = () => {
+    this.setState({ isOpen: !this.state.isOpen });
+  };
 
-    render() {
-        return (
-            <nav className="navbar">
-                <div className="nav-center">
-                    <div className="nav-header">
-                        <Link to="/" >
-                            <img className="navbar-logo" src={logo} alt="Beach" />    
-                        </Link>
-                        <button type="button" className="nav=btn" onClick={this.handleToggle}>
+  render() {
+    return (
+      <nav className="navbar">
+        <div className="nav-center">
+          <div className="nav-header">
+            <Link to="/">
+              <img className="navbar-logo" src={logo} alt="Beach" />
+            </Link>
+            {/* <button type="button" className="nav=btn" onClick={this.handleToggle}>
                             <FaAlignRight className="nav-icon" />
-                        </button>   
-                    </div>
-                    <ul className={this.state.isOpen ? "nav-links show-nav" : "nav-links"}>
-                        <li><Link to="/">Home</Link></li>
-                        <li><Link to="/rooms">Rooms</Link></li>
-                    </ul>
-                </div>
-            </nav>
-        )
-    }
+                        </button>    */}
+          </div>
+          <ul
+            className={this.state.isOpen ? "nav-links show-nav" : "nav-links"}
+          >
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/rooms">Rooms</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
 }
